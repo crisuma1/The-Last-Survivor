32db6389c8065ada8db5e8677c91ba31c69b5ccb
minjun <minhyuck6766@naver.com>
2025-07-29 01:15:46 +0900

    포위몬스터제작

---
+++ Assets/Scripts/monsterTrigger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monsterTrigger : MonoBehaviour
+{
+    public GameObject ZombiePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Player")
+        {
+            Invoke("SpawnZombie", 3f);
+            
+        }
+    }
+    void SpawnZombie()
+    {
+        Instantiate(ZombiePrefab, transform.position, Quaternion.identity);
+    }
+}
