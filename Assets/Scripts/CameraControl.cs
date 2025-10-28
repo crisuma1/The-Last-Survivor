be8396b3b35bb255103447ede49c44759672f48f
crisuma1 <minhyuck6766@naver.com>
2025-10-28 13:47:31 +0900

    카메라버그수정

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -12,25 +12,34 @@ public class CameraControl : MonoBehaviour
 
     float mouseX;
     float mouseY;
-    float offsetDistanceY;
-    float offsetDistanceZ;
+
     public Transform player;
     private bool isClicked = false;
+
+
+
+    //카메라충돌때사용할변수
+    [SerializeField] private float sphereRadius = 0.3f;
+    [SerializeField] private float minDistance = 1f;
+    [SerializeField] private float smoothSpeed = 10f;
+    private Vector3 direction;
+    private float desiredDistance;
+    private Vector3 desiredPosition;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = (Camera.main.transform.position - player.position).normalized;
+        desiredDistance = Vector3.Distance(Camera.main.transform.position, player.position);
+
         // 카메라 초기 회전을 마우스 값으로 세팅
         Vector3 angles = transform.eulerAngles;
         mouseX = angles.y;
-        mouseY = -angles.x;
+        mouseY = -angles.x;     
         
 
 
-        offsetDistanceY = this.transform.position.y- player.position.y;
-        offsetDistanceZ= this.transform.position.z - player.position.z;
         // 커서 숨기기
         if (!clickToMoveCamera)
         {
@@ -38,7 +47,7 @@ public class CameraControl : MonoBehaviour
             UnityEngine.Cursor.visible = false;
         }
       
-
+        
 
     }
 
@@ -52,7 +61,7 @@ public class CameraControl : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(-mouseY, mouseX, 0);
         //플레이어 카메라에맞게 회전(180도 회전)
-        player.rotation = Quaternion.Euler(0, mouseX+180, 0);
+        player.parent.rotation = Quaternion.Euler(0, mouseX+180, 0);
        
     }
 
@@ -64,23 +73,14 @@ public class CameraControl : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             isClicked = true;
-            
         }
-
-
-        // 카메라 위치를 플레이어 위치에 맞게 갱신
-        transform.position = player.position + new Vector3(0, offsetDistanceY, offsetDistanceZ);
 
         //카메라 줌인 줌아웃
         if (canZoom && Input.GetAxis("Mouse ScrollWheel") != 0)
             Camera.main.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * sensitivity * 2;
-      
-
-
 
         if(clickToMoveCamera&& Input.GetAxisRaw("Fire2") != 0)
         {
-
             CalculateCameraPosition();
         }
 
@@ -89,7 +89,22 @@ public class CameraControl : MonoBehaviour
             CalculateCameraPosition();
         }
 
+        direction = transform.forward;//Camera.main.transform.position - player.position;
+        //direction.Normalize();
 
-
+        //카메라와 플레이어사이에 물체가있어서 가려질시 앞당김
+        if (Physics.SphereCast(player.position, sphereRadius, direction, out RaycastHit hit, desiredDistance))
+        {
+            Debug.Log("T");
+            float newDist = Mathf.Clamp(hit.distance - 0.1f, minDistance, desiredDistance);
+            Vector3 newPos = player.position + (direction * newDist);
+            Camera.main.transform.position = newPos;//Vector3.Lerp(Camera.main.transform.position, newPos, Time.deltaTime * smoothSpeed);
+        }
+        else
+        {
+            Debug.Log("F");
+            desiredPosition = player.position + (direction * desiredDistance);
+            Camera.main.transform.position = desiredPosition;//Vector3.Lerp(Camera.main.transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
+        }
     }
 }
