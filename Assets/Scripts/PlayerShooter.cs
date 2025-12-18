4c3f01de17beef480a54ebe0168cf4dc74249775
crisuma1 <minhyuck6766@naver.com>
2025-12-18 16:22:43 +0900

    IK수정및총부착수정완료
    IK수정하여 한쪽만적용및반대쪽손은 총의부모로설정

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -176,11 +176,13 @@ public class PlayerShooter : MonoBehaviour {
     // 애니메이터의 IK 갱신
     private void OnAnimatorIK(int layerIndex) {
         // 총의 기준점 gunPivot을 3D 모델의 오른쪽 팔꿈치 위치로 이동
-        gunPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
+       // gunPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
         //Debug.Log(playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow));
 
 
         // IK를 사용하여 왼손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
+
+       
         playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
         playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
 
@@ -189,16 +191,19 @@ public class PlayerShooter : MonoBehaviour {
         playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand,
             leftHandMount.rotation);
 
-        // IK를 사용하여 오른손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
-        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
-        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
 
-        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand,
-            rightHandMount.position);
-        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand,
-            rightHandMount.rotation);
 
-        
+        /*
+   //IK를 사용하여 오른손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
+   playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+   playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+   playerAnimator.SetIKPosition(AvatarIKGoal.RightHand,
+       rightHandMount.position);
+   playerAnimator.SetIKRotation(AvatarIKGoal.RightHand,
+       rightHandMount.rotation);
+*/
+
 
         /*
         //crosshair의위치를총에따라다르게설정
